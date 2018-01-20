97508ecaf0cf5c0df6c492667ae57f244326d70f
Gareth Funk <gareth.funk94@gmail.com>
2018-01-20 13:52:26 +0000

    Added stuff

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
 # This file is the main one...
+
+print("hello")
+
+# Read in the latex file and parse it
+
+class document:
+    def __init__(self):
+        self.sections = []
+        self.graphics = []
+        self.preamble = ""
+        return
+
+class section:
+    def __init__(self):
+        self.subsections = []
+        self.content = ""
+        return
+
+class subsection:
+    def __init__(self):
+        self.subsubsections = []
+        self.content = ""
+
+class subsubsection:
+    def __init__(self):
+        self.content = ""
+
+class graphic:
+    def __init__(self):
+        self.content = ""
+
+
